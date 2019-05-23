9194b40a8d1ea305d9dfc295ce893740981aee89
@TheRealMcleod <48032469+ChrisCottle112@users.noreply.github.com>
2019-05-23 14:49:42 +1200

    Add files via upload

---
+++ Game of God V2.py
@@ -0,0 +1,48 @@
+from appJar import gui
+import winsound
+
+#2019/3/04
+
+def enterData(button):
+    if button == "Enter":
+        name = app.getEntry("Name")
+        info = "Name : {}".format(name)
+            
+        players.append(Player(name))
+            
+        print(info)
+        app.setLabel("Welcome","Welcome "+name)
+
+
+def launch(start):
+    if start == "Game Of Gods":
+        app.showSubWindow("GOD")
+    if start == "Exit": 
+        app.stop()
+    if start == "Exit.":
+        app.stop()    
+app = gui("Game Of Gods", "fullscreen")
+
+app.addButtons(["Game Of Gods"],launch)   
+app.setBg("black", override=False, tint=False)
+app.setFg("white", override=False)
+app.setFont(15)
+
+app.startSubWindow("GOD", modal=True)
+
+
+"""
+sound is play when opening the game for Assessment file but not when in Wing?
+app.playSound("wii.wav", wait=False)
+app.playSound("bg.wav", wait=False)
+app.playSound("got.wav", wait=False)
+app.playSound("dbs.wav", wait=False)
+"""
+app.setBg("Black")
+app.setSize("fullscreen")
+app.addLabel("Whats your name?")
+app.addLabelEntry("Name")
+app.addButtons(["Enter"],enterData)       
+app.addButtons(["Exit"],launch)
+
+app.go()
